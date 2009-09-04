2b13146726770613a27df90f5ade14bc71ed24f1
shibason <shiba@shiba.skr.jp>
2009-09-05 02:27:40 +0900

    cssを追加したり、テーブル一覧表示のアクション名を変更したり

--- application.rb
+++ application.rb
@@ -1,12 +1,18 @@
 require 'rubygems'
 require 'sinatra'
 require 'haml'
+require 'sass'
 require 'sequel'
 require 'yaml'
 
+configure do
+  set :config, YAML.load_file('config.yml')
+  set :pagesize, 30
+end
+
 helpers do
   def db_connect(&block)
-    Sequel.connect(@config['database'], &block)
+    Sequel.connect(options.config['database'], &block)
   end
 
   def link_to(path = '/', parameters = nil)
@@ -18,44 +24,50 @@ end
 
 before do
   request.script_name = request.script_name.sub(%r|/dispatch.cgi$|, '')
-  @config = YAML.load_file('config.yml')
+  headers('Cache-Control' => 'no-cache',
+          'Pragma' => 'no-cache',
+          'Expires' => '0')
   @index_url = link_to
+  @css_url = link_to('/resource/css')
 end
 
 get '/' do
   db_connect do |db|
     @tables = db.tables.map do |table|
-      {
-        :name => table,
-        :link => link_to("/#{table}"),
-      }
+      { :name => table, :link => link_to("/list/#{table}") }
     end
   end
+  @page_title = 'List of tables'
   haml :index
 end
 
-PAGESIZE = 30
-get '/:table' do
-  @table = params[:table]
-  @page = params[:page].to_i
+get %r|/list/([^/?&#]+)(?:/([^/?&#]+))?| do |table, page|
+  @table = table
+  @page = page.to_i
   @page = 1 if @page < 1
 
   db_connect do |db|
     dataset = db[@table.to_sym]
     @count = dataset.count
     @columns = dataset.columns
-    @rows = dataset.limit(PAGESIZE, (@page - 1) * PAGESIZE).all
+    @rows = dataset.limit(options.pagesize, (@page - 1) * options.pagesize).all
   end
 
-  @max_page = @count / PAGESIZE + 1
+  @max_page = @count / options.pagesize + 1
   if @page == 2
-    @prev_url = link_to("/#{@table}")
+    @prev_url = link_to("/list/#{@table}")
   elsif @page > 2
-    @prev_url = link_to("/#{@table}", :page => @page - 1)
+    @prev_url = link_to("/list/#{@table}/#{@page - 1}")
   end
-  @next_url = link_to("/#{@table}", :page => @page + 1) if @page < @max_page
+  @next_url = link_to("/list/#{@table}/#{@page + 1}") if @page < @max_page
 
+  @page_title = "List of #{@table}"
   haml :list
+end
+
+get '/resource/css' do
+  content_type 'text/css', :charset => 'utf-8'
+  sass :css
 end
 
 __END__
@@ -64,19 +76,22 @@ __END__
 !!!
 %html
   %head
+    %meta{ 'http-equiv' => 'Content-Type', |
+           :content => 'text/html; charset=UTF-8' }
     %title= @page_title
+    %link{ :rel => 'stylesheet', :type => 'text/css', :href => @css_url }
   %body
     = yield
 
 @@index
-%h2 Tables
+%h2= @page_title
 %ul
   - @tables.each do |table|
     %li
       %a{ :href => table[:link] }= table[:name]
 
 @@list
-%h2 List of #{@table}
+%h2= @page_title
 %table
   %tr
     - @columns.each do |column|
@@ -94,4 +109,15 @@ __END__
     - if @next_url
       %a{ :href => @next_url } Next
 %p
-  %a{ :href => @index_url } To Index
+  %a{ :href => @index_url } Index
+
+@@css
+body
+  :margin 20px
+table
+  :border-collapse collapse
+th
+  :background-color #f5f5f5
+th, td
+  :padding 0.2em
+  :border 1px solid #808080
